d6daf3d7af9dc344d4d4314a1b61ae9edcbb9c75
FadhilAF <falfatih3421@gmail.com>
2023-05-14 10:14:27 +0700

    update session idle time and lifetime

--- common/session/init.go
+++ common/session/init.go
@@ -19,8 +19,8 @@ var SessionManager *scs.SessionManager
 func SessionHandler(handler *gin.Engine, dbPool *pgxpool.Pool, config env.Config) http.Handler {
 	SessionManager = scs.New()
 	SessionManager.Store = pgxstore.New(dbPool)
-	SessionManager.Lifetime = 12 * time.Hour
-	SessionManager.IdleTimeout = 20 * time.Minute
+	SessionManager.Lifetime = 24 * time.Hour
+	SessionManager.IdleTimeout = 3 * time.Hour
 	SessionManager.Cookie.Persist = true
 	SessionManager.Cookie.HttpOnly = true
 	SessionManager.Cookie.Path = "/"
