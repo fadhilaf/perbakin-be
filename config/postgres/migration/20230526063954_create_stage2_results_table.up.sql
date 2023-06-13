48f8e7dbe08befa9de34d5cc89831838c8efa118
FadhilAF <falfatih3421@gmail.com>
2023-06-13 23:32:03 +0700

    update stage 1 stage 2 stage 3 DDL

--- config/postgres/migration/20230526063954_create_stage2_results_table.up.sql
+++ config/postgres/migration/20230526063954_create_stage2_results_table.up.sql
@@ -6,34 +6,20 @@ CREATE TYPE stage246_checkmarks AS (
 
 CREATE TYPE stage246_status AS ENUM ('1', '2', '3', '4');
 
-CREATE TYPE stage2_tries AS (
-  status stage246_status,
-  no1 stage123_numbers,
-  no2 stage123_numbers,
-  no3 stage123_numbers,
-  checkmarks stage246_checkmarks
+CREATE TABLE IF NOT EXISTS stage2_tries (
+  id uuid PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+  status stage246_status NOT NULL DEFAULT '1',
+  no1 stage123_numbers NOT NULL DEFAULT (ROW(0, 0, 0), ROW(0, 0, 0)),
+  no2 stage123_numbers NOT NULL DEFAULT (ROW(0, 0, 0), ROW(0, 0, 0)),
+  no3 stage123_numbers NOT NULL DEFAULT (ROW(0, 0, 0), ROW(0, 0, 0)),
+  checkmarks stage246_checkmarks NOT NULL DEFAULT (ROW(false, false, false))
 );
 
 CREATE TABLE IF NOT EXISTS stage2_results (
   id uuid PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
   result_id uuid NOT NULL UNIQUE,
-  try1 stage2_tries NOT NULL DEFAULT ROW( 
-    '1',
-    ROW(
-      ROW(0,0,0),
-      ROW(0,0,0)
-    ),
-    ROW(
-      ROW(0,0,0),
-      ROW(0,0,0)
-    ),
-    ROW(
-      ROW(0,0,0),
-      ROW(0,0,0)
-    ),
-    ROW(false,false,false)
-  ),
-  try2 stage2_tries DEFAULT NULL,
+  try1_id uuid NOT NULL UNIQUE,
+  try2_id uuid DEFAULT NULL UNIQUE,
   is_try2 boolean NOT NULL DEFAULT FALSE,
   shooter_sign varchar(255) DEFAULT NULL,
   scorer_sign varchar(255) DEFAULT NULL,
@@ -41,5 +27,11 @@ CREATE TABLE IF NOT EXISTS stage2_results (
   updated_at timestamp NOT NULL DEFAULT NOW(),
   CONSTRAINT result_id
     FOREIGN KEY (result_id) 
-      REFERENCES results (id) ON DELETE CASCADE
+      REFERENCES results (id) ON DELETE CASCADE,
+  CONSTRAINT try1_id 
+    FOREIGN KEY (try1_id) 
+      REFERENCES stage2_tries (id) ON DELETE CASCADE,
+  CONSTRAINT try2_id 
+    FOREIGN KEY (try2_id) 
+      REFERENCES stage2_tries (id) ON DELETE SET NULL
 );
--- config/postgres/migration/20230526063958_create_stage3_results_table.up.sql
+++ config/postgres/migration/20230526063958_create_stage3_results_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS stage3_results (
   id uuid PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
   result_id uuid NOT NULL UNIQUE,
-  try1 uuid NOT NULL,
-  try2 uuid DEFAULT NULL,  
+  try1_id uuid NOT NULL UNIQUE,
+  try2_id uuid DEFAULT NULL UNIQUE,
   is_try2 boolean NOT NULL DEFAULT FALSE,
   shooter_sign varchar(255) DEFAULT NULL,
   scorer_sign varchar(255) DEFAULT NULL,
@@ -10,5 +10,11 @@ CREATE TABLE IF NOT EXISTS stage3_results (
   updated_at timestamp NOT NULL DEFAULT NOW(),
   CONSTRAINT result_id
     FOREIGN KEY (result_id) 
-      REFERENCES results (id) ON DELETE CASCADE
+      REFERENCES results (id) ON DELETE CASCADE,
+  CONSTRAINT try1_id 
+    FOREIGN KEY (try1_id) 
+      REFERENCES stage13_tries (id) ON DELETE CASCADE,
+  CONSTRAINT try2_id 
+    FOREIGN KEY (try2_id) 
+      REFERENCES stage13_tries (id) ON DELETE SET NULL
 );
